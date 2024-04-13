d9b4468f21d38a660a356424ad2645bbb0ae26fd
Xyber Pastoril <github.ninja458@passfwd.com>
2024-04-13 16:05:10 +0800

    Hotfix: Links return multiple redirects despite working fine in local

--- src/routes/+page.server.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { redirect } from '@sveltejs/kit';
-import { error } from '@sveltejs/kit';
-
-export async function load({  }) {
-    
-    // error(res.status_code, res.status_message);
-    throw redirect(302, `https://github.com/xybr-dev`);
-}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { onMount } from "svelte";
+    import Redirect from "$lib/Redirect.svelte";
+
+    onMount(() => {
+
+    })
+</script>
+
+<Redirect href="https://github.com/xybr-dev" />
