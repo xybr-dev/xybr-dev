61541fc9e93692fa4349117f29b494455d5a6bc1
Xyber Pastoril <connect@xybr.dev>
2024-11-09 15:00:42 +0800

    Update savedbygrace email

--- src/routes/savedbygrace/+page.server.js
+++ src/routes/savedbygrace/+page.server.js
@@ -2,5 +2,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ }) {
-    throw redirect(302, `mailto:savedbygrace@xybr.dev`);
+    throw redirect(302, `mailto:savedbygrace@mail.xybr.dev`);
 }
